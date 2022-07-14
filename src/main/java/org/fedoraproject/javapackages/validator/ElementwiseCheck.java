48639e5492949b69b6cfdff0a36fb359be255297
Marián Konček <marian.koncek@mailbox.org>
2022-07-14 17:12:42 +0200

    Check for null config

--- src/main/java/org/fedoraproject/javapackages/validator/ElementwiseCheck.java
+++ src/main/java/org/fedoraproject/javapackages/validator/ElementwiseCheck.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 import org.fedoraproject.javadeptools.rpm.RpmInfo;
@@ -11,13 +12,22 @@ import org.fedoraproject.javadeptools.rpm.RpmInfo;
 public abstract class ElementwiseCheck<Config> extends Check<Config> {
     abstract protected Collection<String> check(Path rpmPath, RpmInfo rpmInfo, Config config) throws IOException;
 
+    private Collection<String> checkNonNull(Path rpmPath, RpmInfo rpmInfo, Config config) throws IOException {
+        if (config == null) {
+            System.err.println("[INFO] Configuration class not found, ignoring the test");
+            return Collections.emptyList();
+        }
+
+        return check(rpmPath, rpmInfo, config);
+    }
+
     @Override
     protected Collection<String> check(List<Path> testRpms, Config config) throws IOException {
         var result = new ArrayList<String>(0);
 
         for (Path rpmPath : testRpms) {
             var rpmInfo = new RpmInfo(rpmPath);
-            result.addAll(check(rpmPath, rpmInfo, config));
+            result.addAll(checkNonNull(rpmPath, rpmInfo, config));
         }
 
         return result;
