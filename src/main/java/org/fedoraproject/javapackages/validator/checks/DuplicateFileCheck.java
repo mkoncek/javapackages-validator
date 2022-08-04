e843aad6bb65bc3c1a1d37d3241236b3a187816d
Marián Konček <marian.koncek@mailbox.org>
2022-08-04 16:01:34 +0200

    Ignore duplicate directories

--- src/main/java/org/fedoraproject/javapackages/validator/checks/DuplicateFileCheck.java
+++ src/main/java/org/fedoraproject/javapackages/validator/checks/DuplicateFileCheck.java
@@ -7,6 +7,8 @@ import java.util.Collection;
 import java.util.Iterator;
 import java.util.TreeMap;
 
+import org.apache.commons.compress.archivers.cpio.CpioArchiveEntry;
+import org.apache.commons.lang3.tuple.Pair;
 import org.fedoraproject.javadeptools.rpm.RpmInfo;
 import org.fedoraproject.javapackages.validator.Check;
 import org.fedoraproject.javapackages.validator.Common;
@@ -33,13 +35,14 @@ public class DuplicateFileCheck extends Check<DuplicateFileConfig> {
         var result = new ArrayList<String>(0);
 
         // The union of file paths present in all RPM files mapped to the RPM file names they are present in
-        var files = new TreeMap<String, ArrayList<Path>>();
+        var files = new TreeMap<String, ArrayList<Pair<CpioArchiveEntry, Path>>>();
 
         while (testRpms.hasNext()) {
             RpmPathInfo rpm = testRpms.next();
             if (!new RpmInfo(rpm.getPath()).isSourcePackage()) {
                 for (var pair : Common.rpmFilesAndSymlinks(rpm.getPath()).entrySet()) {
-                    files.computeIfAbsent(pair.getKey().getName().substring(1), key -> new ArrayList<Path>()).add(rpm.getPath());
+                    files.computeIfAbsent(Common.getEntryPath(pair.getKey()).toString(), key -> new ArrayList<>())
+                        .add(Pair.of(pair.getKey(), rpm.getPath()));
                 }
             }
         }
@@ -47,30 +50,37 @@ public class DuplicateFileCheck extends Check<DuplicateFileConfig> {
         for (var entry : files.entrySet()) {
             if (entry.getValue().size() > 1) {
                 var providers = new ArrayList<RpmPackageInfo>(entry.getValue().size());
-                for (var providerRpmPath : entry.getValue()) {
-                    providers.add(new RpmPackageInfo(providerRpmPath));
+                for (var providerPair : entry.getValue()) {
+                    providers.add(new RpmPackageInfo(providerPair.getValue()));
                 }
-                var ok = new Boolean[] {true};
+                var okDifferentArchs = new Boolean[] {true};
+                // If all providers are of different architecture (with the
+                // exception of noarch), then it is ok
                 providers.sort((lhs, rhs) -> {
                     int cmp = lhs.getArch().compareTo(rhs.getArch());
                     if (cmp == 0 || lhs.getArch().equals("noarch") || rhs.getArch().equals("noarch")) {
-                        ok[0] = false;
+                        okDifferentArchs[0] = false;
                     }
                     return cmp;
                 });
-                String decoratedProviders = entry.getValue().stream().map(path -> Main.getDecorator().decorate(path, Decoration.bright_red)).toList().toString();
-                if (ok[0]) {
+
+                // If the file entry is a directory in all providers, then it is ok
+                boolean okDirectory = entry.getValue().stream().map(Pair::getKey).allMatch(CpioArchiveEntry::isDirectory);
+
+                String decoratedFile = Main.getDecorator().decorate(entry.getKey(), Decoration.bright_cyan);
+                String decoratedProviders = entry.getValue().stream().map(pair -> Main.getDecorator().decorate(pair.getValue(), Decoration.bright_red)).toList().toString();
+                if (okDifferentArchs[0]) {
                     getLogger().pass("File {0} provided by RPMs of unique architectures: {1}",
-                            Main.getDecorator().decorate(entry.getKey(), Decoration.bright_cyan),
-                            decoratedProviders);
+                            decoratedFile, decoratedProviders);
+                } else if (okDirectory) {
+                    getLogger().pass("File {0} is a directory provided by multiple RPMs: {1}",
+                            decoratedFile, decoratedProviders);
                 } else if (getConfig() != null && getConfig().allowedDuplicateFile(entry.getKey(), providers)) {
                     getLogger().pass("Allowed duplicate file {0} provided by multiple RPMs: {1}",
-                            Main.getDecorator().decorate(entry.getKey(), Decoration.bright_cyan),
-                            decoratedProviders);
+                            decoratedFile, decoratedProviders);
                 } else {
                     result.add(failMessage("File {0} provided by multiple RPMs: {1}",
-                            Main.getDecorator().decorate(entry.getKey(), Decoration.bright_cyan),
-                            decoratedProviders));
+                            decoratedFile, decoratedProviders));
                 }
             }
         }
