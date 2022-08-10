b692507d87a00cf6af4dbdf3e7ba76143c1e0610
Marián Konček <marian.koncek@mailbox.org>
2022-08-10 14:13:23 +0200

    Add more info to JAR manifest, simplify run script

--- run.sh
+++ run.sh
@@ -4,9 +4,4 @@ set -e
 
 java_bin='/usr/lib/jvm/jre-17/bin'
 
-classpath="$(echo target/*.jar)"
-for dependency in target/dependency/*; do
-    classpath+=":${dependency}"
-done
-
-exec ${java_bin}/java --enable-preview --add-modules jdk.incubator.foreign --enable-native-access ALL-UNNAMED -cp "${classpath}" 'org.fedoraproject.javapackages.validator.Main' ${@}
+exec "${java_bin}"/java --enable-preview --add-modules jdk.incubator.foreign --enable-native-access ALL-UNNAMED -jar "$(ls target/*.jar | tail -n1)" ${@}
