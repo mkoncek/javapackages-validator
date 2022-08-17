d3e72f85e1b9c1972cb7ce6b67f4524970e444ba
Marián Konček <marian.koncek@mailbox.org>
2022-08-17 12:57:37 +0200

    Add default implementation of exclusive arch config

--- src/main/java/org/fedoraproject/javapackages/validator/config/ExclusiveArchConfig.java
+++ src/main/java/org/fedoraproject/javapackages/validator/config/ExclusiveArchConfig.java
@@ -5,5 +5,19 @@ import java.util.List;
 import org.fedoraproject.javadeptools.rpm.RpmInfo;
 
 public interface ExclusiveArchConfig {
+    /**
+     * @param rpm The rpm package.
+     * @param values The values of exclusive arch field.
+     * @return Whether the rpm package has passed with given values of exclusive arch.
+     */
     boolean allowedExclusiveArch(RpmInfo rpm, List<String> values);
+
+    public static class Default implements ExclusiveArchConfig {
+        @Override
+        public boolean allowedExclusiveArch(RpmInfo rpm, List<String> values) {
+            boolean buildNoarch = rpm.getBuildArchs().contains("noarch");
+            boolean exclusiveNoarch = values.contains("noarch");
+            return buildNoarch == exclusiveNoarch;
+        }
+    }
 }
