65c945ff6daa2b029449908ed82553b2037070b6
Marián Konček <marian.koncek@mailbox.org>
2022-08-17 12:57:04 +0200

    Update comment

--- src/main/java/org/fedoraproject/javapackages/validator/config/DuplicateFileConfig.java
+++ src/main/java/org/fedoraproject/javapackages/validator/config/DuplicateFileConfig.java
@@ -8,7 +8,7 @@ public interface DuplicateFileConfig {
     /**
      * @param filename The absolute path of the file.
      * @param providerRpms Non-source RPM packages which provide the file.
-     * Will always contain more than one entry.
+     * Always contains more than one entry.
      * @return Error messages.
      */
     boolean allowedDuplicateFile(String filename, Collection<? extends RpmInfo> providerRpms);
