81a635c436c56ce711e631a14bdfcf3ed197a3cf
Marián Konček <marian.koncek@mailbox.org>
2022-08-22 13:07:20 +0200

    Make CI use tmt tests

--- ci_test.sh
+++ ci_test.sh
@@ -4,9 +4,12 @@ set -ex
 
 jp_validator_image='javapackages-validator'
 test_artifacts_dir='/opt/test_artifacts'
-jpv_tests_dir='/opt/javapackages'
+jpv_tests_dir='/opt/javapackages-validator-tests'
+jpv_tests_url='https://pagure.io/javapackages-validator-tests'
 
 build_local_image() {
+    # Add colored and debug outputs
+    sed -i 's|ENTRYPOINT \["./run.sh"\]|ENTRYPOINT ["./run.sh", "-r", "-x"]|' Dockerfile.main
     podman build -f Dockerfile.main -t "${jp_validator_image}"
 }
 
@@ -17,8 +20,16 @@ download_ci_env() {
 
 prepare_test_env() {
     download_ci_env
-    git -C "${jpv_tests_dir%/*}" clone 'https://src.fedoraproject.org/tests/javapackages.git'
-    git -C "${jpv_tests_dir}" checkout '8df63c0d82316139507a70fdf93430c63bbff8bf'
+    git -C "${jpv_tests_dir%/*}" clone "${jpv_tests_url}.git"
+    git -C "${jpv_tests_dir}" checkout 'cc9538d898bd000ceeef53d8cb4009701ad172dd'
+    find "${test_artifacts_dir}" -mindepth 3 -maxdepth 3 -wholename '*/*/plans/javapackages.fmf' -exec sed -i "s|url: ${jpv_tests_url}|path: ${jpv_tests_dir}|" {} +
+    
+    # https://github.com/teemtee/tmt/pull/1349
+    sudo rm /bin/sh
+    sudo ln -s /bin/bash /bin/sh
+}
+
+execute_symlink_check() {
     echo "
 package org.fedoraproject.javapackages.validator.config;
 
@@ -26,19 +37,34 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
-public class SymlinkConfigF37 extends SymlinkConfig.RpmSet {
-    public SymlinkConfigF37() throws IOException {
+public class SymlinkConfigJP extends SymlinkConfig.RpmSetImpl {
+    public SymlinkConfigJP() throws IOException {
         super(Files.find(Paths.get(\"/mnt/test_artifacts\"), Integer.MAX_VALUE,
                 (path, attributes) -> !attributes.isDirectory() && path.toString().endsWith(\".rpm\")).iterator());
     }
 }
-" > "${jpv_tests_dir}/src/config/SymlinkConfigJP.java"
+" > "${jpv_tests_dir}/src/SymlinkConfigJP.java"
+    pushd "${jpv_tests_dir}"
+    TEST_ARTIFACTS="${test_artifacts_dir}" JP_VALIDATOR_IMAGE="${jp_validator_image}" ./jp_validator.sh -r -x SymlinkCheck -c /mnt/config/SymlinkConfigJP.java
+    popd
+    echo "public class SymlinkConfigJP {}" > "${jpv_tests_dir}/src/SymlinkConfigJP.java"
 }
 
 execute() {
-    find "${jpv_tests_dir}"
-    cd ${jpv_tests_dir}/tmt
-    JAVAPACKAGES_VALIDATOR_IMAGE="${jp_validator_image}" TEST_ARTIFACTS="${test_artifacts_dir}" bash -x ./jp_validator.sh -r -x 'All' -c "${jpv_tests_dir}/src/config"
+    execute_symlink_check
+    
+    for component in $(ls ${test_artifacts_dir}); do
+        if [ -f "${test_artifacts_dir}/${component}/plans/javapackages.fmf" ]; then
+            tmt --root "${test_artifacts_dir}/${component}" run \
+                -e TEST_ARTIFACTS="${test_artifacts_dir}/${component}"\
+                -e JP_VALIDATOR_IMAGE="${jp_validator_image}"\
+                discover --how fmf --path "${jpv_tests_dir}"\
+                provision --how local\
+                execute\
+                report -vvv\
+                plan --name /plans/javapackages
+        fi
+    done
 }
 
 if [ "${1}" = 'build_local_image' ]; then
