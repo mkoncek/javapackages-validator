5db858fd6421a87fa81afbc413938e5e055c3fbf
Mikolaj Izdebski <mizdebsk@redhat.com>
2022-08-22 18:50:55 +0200

    [ci] Display structurized output in GitHub Workflow
    See:
    https://docs.github.com/en/actions/using-workflows/workflow-commands-for-github-actions#grouping-log-lines

--- ci_test.sh
+++ ci_test.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-
-set -ex
+set -eu
 
 jp_validator_image='javapackages-validator'
 test_artifacts_dir='/tmp/test_artifacts'
@@ -9,8 +8,6 @@ jpv_tests_url='https://pagure.io/javapackages-validator-tests'
 jpv_tests_ref='17acc96ee933e080640f244918bd12e74b92aae2'
 
 build_local_image() {
-    # Add colored and debug outputs
-    sed -i 's|ENTRYPOINT \["./run.sh"\]|ENTRYPOINT ["./run.sh", "-r", "-x"]|' Dockerfile.main
     podman build -f Dockerfile.main -t "${jp_validator_image}"
 }
 
@@ -26,17 +23,37 @@ prepare_test_env() {
 
 execute() {
     for component in $(ls ${test_artifacts_dir}/rpms); do
-        if [ -f "${test_artifacts_dir}/rpms/${component}/plans/javapackages.fmf" ]; then
-            tmt --root "${test_artifacts_dir}/rpms/${component}" run \
-                -e TEST_ARTIFACTS="${test_artifacts_dir}/rpms/${component}/rpms"\
-                -e JP_VALIDATOR_IMAGE="${jp_validator_image}"\
-                -e ENVROOT="${test_artifacts_dir}/envroot"\
-                discover --how fmf --url "${jpv_tests_dir}" --ref "${jpv_tests_ref}"\
-                provision --how local\
-                execute\
-                report -vvv\
-                plan --name /plans/javapackages
+        run_id="jpv-ci-${component}"
+        echo "::group::Run tests for ${component}"
+
+        # Discover tests
+        if ! tmt -q --root "${test_artifacts_dir}/rpms/${component}" \
+             run --scratch \
+                 --id "${run_id}" \
+             discover --how fmf \
+                      --url "${jpv_tests_dir}" \
+                      --ref "${jpv_tests_ref}" \
+             plan --name /plans/javapackages
+        then
+            echo "::endgroup::"
+            continue
         fi
+
+        # Execute tests
+        if tmt -q \
+           run --id "${run_id}" \
+               -e TEST_ARTIFACTS="${test_artifacts_dir}/rpms/${component}/rpms" \
+               -e JP_VALIDATOR_IMAGE="${jp_validator_image}" \
+               -e ENVROOT="${test_artifacts_dir}/envroot" \
+           provision --how local \
+           execute --how tmt --no-progress-bar
+        then
+            echo "::endgroup::"
+            continue
+        fi
+
+        # Display test report if testing failed
+        tmt -vvv run --id "${run_id}" report
     done
 }
 
