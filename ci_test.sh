70a0f3ef68563184e67dcb06c29ec35e6eed71f1
Mikolaj Izdebski <mizdebsk@redhat.com>
2022-08-22 17:25:57 +0200

    [ci] Execute symlink check using envroot configs
    Stop using custom symlink check in favor of standard check based on
    envroot.

--- ci_test.sh
+++ ci_test.sh
@@ -21,39 +21,17 @@ download_ci_env() {
 prepare_test_env() {
     download_ci_env
     git -C "${jpv_tests_dir%/*}" clone "${jpv_tests_url}.git"
-    git -C "${jpv_tests_dir}" checkout 'cc9538d898bd000ceeef53d8cb4009701ad172dd'
+    git -C "${jpv_tests_dir}" checkout '17acc96ee933e080640f244918bd12e74b92aae2'
     find "${test_artifacts_dir}/rpms" -mindepth 3 -maxdepth 3 -wholename '*/*/plans/javapackages.fmf' -exec sed -i "s|url: ${jpv_tests_url}|path: ${jpv_tests_dir}|" {} +
 }
 
-execute_symlink_check() {
-    echo "
-package org.fedoraproject.javapackages.validator.config;
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-
-public class SymlinkConfigJP extends SymlinkConfig.RpmSetImpl {
-    public SymlinkConfigJP() throws IOException {
-        super(Files.find(Paths.get(\"/mnt/test_artifacts\"), Integer.MAX_VALUE,
-                (path, attributes) -> !attributes.isDirectory() && path.toString().endsWith(\".rpm\")).iterator());
-    }
-}
-" > "${jpv_tests_dir}/src/SymlinkConfigJP.java"
-    pushd "${jpv_tests_dir}"
-    TEST_ARTIFACTS="${test_artifacts_dir}/rpms" JP_VALIDATOR_IMAGE="${jp_validator_image}" ./jp_validator.sh -r -x SymlinkCheck -c /mnt/config/SymlinkConfigJP.java
-    popd
-    echo "public class SymlinkConfigJP {}" > "${jpv_tests_dir}/src/SymlinkConfigJP.java"
-}
-
 execute() {
-    execute_symlink_check
-    
     for component in $(ls ${test_artifacts_dir}/rpms); do
         if [ -f "${test_artifacts_dir}/rpms/${component}/plans/javapackages.fmf" ]; then
             tmt --root "${test_artifacts_dir}/rpms/${component}" run \
                 -e TEST_ARTIFACTS="${test_artifacts_dir}/rpms/${component}"\
                 -e JP_VALIDATOR_IMAGE="${jp_validator_image}"\
+                -e ENVROOT="${test_artifacts_dir}/envroot"\
                 discover --how fmf --path "${jpv_tests_dir}"\
                 provision --how local\
                 execute\
