039514a2e5f3b488a5cc6f6ebf3fdea0841bf307
Marián Konček <marian.koncek@mailbox.org>
2022-08-24 09:29:40 +0200

    Simplify entrypoint in Dockerfile

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ set -e
 
 java_bin='/usr/lib/jvm/jre-17/bin'
 
-exec "${java_bin}"/java --enable-preview --add-modules jdk.incubator.foreign --enable-native-access ALL-UNNAMED -jar "$(ls target/*.jar | tail -n1)" ${@}
+exec "${java_bin}"/java --enable-preview --add-modules jdk.incubator.foreign --enable-native-access ALL-UNNAMED -jar 'target/validator.jar' ${@}
