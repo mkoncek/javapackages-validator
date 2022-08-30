e5c72722d1874509ed4ea46046f21bfa77f62af4
Mikolaj Izdebski <mizdebsk@redhat.com>
2022-08-30 14:35:11 +0200

    Further simplify RpmFilesizeCheckTest

--- src/test/java/org/fedoraproject/javapackages/validator/checks/RpmFilesizeCheckTest.java
+++ src/test/java/org/fedoraproject/javapackages/validator/checks/RpmFilesizeCheckTest.java
@@ -1,49 +1,48 @@
 package org.fedoraproject.javapackages.validator.checks;
 
-import static org.easymock.EasyMock.anyObject;
 import static org.easymock.EasyMock.createStrictMock;
-import static org.easymock.EasyMock.eq;
 import static org.easymock.EasyMock.expect;
 import static org.easymock.EasyMock.replay;
 import static org.easymock.EasyMock.verify;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.fedoraproject.javapackages.validator.TestCommon.assertFailOne;
+import static org.fedoraproject.javapackages.validator.TestCommon.assertPass;
 
-import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.stream.Stream;
 
-import org.fedoraproject.javadeptools.rpm.RpmInfo;
 import org.fedoraproject.javapackages.validator.CheckResult;
+import org.fedoraproject.javapackages.validator.RpmPathInfo;
 import org.fedoraproject.javapackages.validator.TestCommon;
 import org.fedoraproject.javapackages.validator.config.RpmFilesizeConfig;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class RpmFilesizeCheckTest {
+    private RpmPathInfo rpmPathInfo;
+    private RpmFilesizeConfig conf;
 
-    Path rpmPath = TestCommon.RPM_PATH_PREFIX.resolve(Paths.get("noarch/duplicate-file1-1-1.noarch.rpm"));
-
-    RpmFilesizeConfig conf = createStrictMock(RpmFilesizeConfig.class);
+    @BeforeEach
+    public void setUp() throws Exception {
+        rpmPathInfo = new RpmPathInfo(
+                TestCommon.RPM_PATH_PREFIX.resolve(Paths.get("noarch/duplicate-file1-1-1.noarch.rpm")));
+        conf = createStrictMock(RpmFilesizeConfig.class);
+    }
 
     private CheckResult runTest() throws Exception {
         replay(conf);
-        var result = new RpmFilesizeCheck().check(conf, TestCommon.iteratorFrom(Stream.of(rpmPath)));
+        var result = new RpmFilesizeCheck().check(conf, rpmPathInfo);
         verify(conf);
         return result;
     }
 
     @Test
     public void testAllowedFileSize() throws Exception {
-        expect(conf.allowedFilesize(anyObject(RpmInfo.class), eq(6488L))).andReturn(true);
-        assertTrue(runTest().isPass());
+        expect(conf.allowedFilesize(rpmPathInfo, 6488L)).andReturn(true);
+        assertPass(runTest());
     }
 
     @Test
     public void testDisallowedFileSize() throws Exception {
-        expect(conf.allowedFilesize(anyObject(RpmInfo.class), eq(6488L))).andReturn(false);
-        var result = runTest();
-        assertFalse(result.isPass());
-        assertEquals(1, result.getFailureCount());
+        expect(conf.allowedFilesize(rpmPathInfo, 6488L)).andReturn(false);
+        assertFailOne(runTest());
     }
 }
