249a03d8b6110156195a19c2abe395c378a70ad1
Marián Konček <marian.koncek@mailbox.org>
2022-11-09 14:51:18 +0100

    Re-add debugging about filtered-out rpms

--- src/main/java/org/fedoraproject/javapackages/validator/validators/ElementwiseValidator.java
+++ src/main/java/org/fedoraproject/javapackages/validator/validators/ElementwiseValidator.java
@@ -4,7 +4,9 @@ import java.io.IOException;
 import java.util.Iterator;
 import java.util.function.Predicate;
 
+import org.fedoraproject.javapackages.validator.Decorated;
 import org.fedoraproject.javapackages.validator.RpmPathInfo;
+import org.fedoraproject.javapackages.validator.TextDecorator.Decoration;
 
 public abstract class ElementwiseValidator extends Validator {
     private Predicate<RpmPathInfo> filter;
@@ -24,6 +26,10 @@ public abstract class ElementwiseValidator extends Validator {
             RpmPathInfo rpm = rpmIt.next();
             if (filter.test(rpm)) {
                 validate(rpm);
+            } else {
+                debug("{0}: filtered out {1}",
+                        Decorated.custom(getClass().getSimpleName(), Decoration.bright_yellow),
+                        Decorated.rpm(rpm));
             }
         }
     }
