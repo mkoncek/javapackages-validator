5796ed16def0a5d2443ff0da8bc4281b9a82b344
Marián Konček <marian.koncek@mailbox.org>
2023-01-05 12:47:32 +0100

    Quote forwarded arguments from shell script

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ set -e
 
 java_bin='/usr/lib/jvm/jre-17/bin'
 
-exec "${java_bin}"/java --enable-preview --add-modules jdk.incubator.foreign --enable-native-access ALL-UNNAMED -jar 'target/validator.jar' ${@}
+exec "${java_bin}"/java --enable-preview --add-modules jdk.incubator.foreign --enable-native-access ALL-UNNAMED -jar 'target/validator.jar' "${@}"
