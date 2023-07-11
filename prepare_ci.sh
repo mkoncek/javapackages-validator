5809c133addf4d7ac2cc8efd9170d5384fd46192
Marián Konček <marian.koncek@mailbox.org>
2023-07-11 15:31:59 +0200

    Update prepare script

--- prepare_ci.sh
+++ prepare_ci.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 
-set -e
+set -eux
 
-for component in $(curl -S 'https://mbi-artifacts.s3.eu-central-1.amazonaws.com/66023ada-9115-4311-b33e-c453150afd51/subject.xml'\
+for component in $(curl -S 'https://mbi-artifacts.s3.eu-central-1.amazonaws.com/f8e6c83c-94ac-43a2-ad7f-9a01aa453c1b/subject.xml'\
     | xmllint --xpath '/subject/component/name/text()' -); do (
     nevra="$(koji latest-pkg rawhide "${component}" | tail -n +3)"
     nevra="${nevra%%[[:space:]]*}"
-    echo "${nevra}"
     dist_git_url="$(koji buildinfo "${nevra}" | grep Extra | sed "s/.*git+\(.*\)'.*/\1/")"
-    echo "${dist_git_url}"
     git clone "${dist_git_url%%#*}"
-    cd "${component}"
+    pushd "${component}"
     git checkout -q "${dist_git_url##*#}"
     
     mkdir rpms
     pushd rpms
     koji download-build --arch noarch --arch x86_64 --arch src --debuginfo "${nevra}"
+    popd
     popd
 ) & done
 wait
