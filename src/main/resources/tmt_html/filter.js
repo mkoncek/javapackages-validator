b6bf6204ded149edfd6c5d6d9a52278857b98de8
Marián Konček <marian.koncek@mailbox.org>
2023-12-06 11:53:32 +0100

    Improve the performance of JQuery script thanks @mizdebsk

--- src/main/resources/tmt_html/filter.js
+++ src/main/resources/tmt_html/filter.js
@@ -4,10 +4,12 @@ $(document).ready(function() {
     $("#checkbox_fail").prop("checked", true);
     $("#checkbox_error").prop("checked", true);
     $(".filter_checkbox").change(function() {
+        $("table").hide();
         if (this.checked) {
             $("." + $(this).val()).show();
         } else {
             $("." + $(this).val()).hide();
         }
+        $("table").show();
     }).change();
 });
