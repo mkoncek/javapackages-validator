dcdf8c3286d14e23d0ed95919d5343502383e133
Marián Konček <marian.koncek@mailbox.org>
2024-07-18 15:36:51 +0200

    Fix JPMS-Provides validator not resetting its state after processing RPM entries

--- src/main/java/org/fedoraproject/javapackages/validator/validators/JpmsProvidesValidator.java
+++ src/main/java/org/fedoraproject/javapackages/validator/validators/JpmsProvidesValidator.java
@@ -77,12 +77,14 @@ public class JpmsProvidesValidator extends JarValidator {
 
     @Override
     public void validate(RpmPackage rpm) throws Exception {
+        jarModuleNames.clear();
         var providedModuleNames = new TreeSet<String>();
 
         for (var reldep : rpm.getInfo().getProvides()) {
             var name = reldep.getName();
             if (name.startsWith("jpms(") && name.endsWith(")")) {
-                providedModuleNames.add(name.substring(5, name.length() - 1));
+                name = name.substring(5, name.length() - 1);
+                providedModuleNames.add(name);
                 debug("{0}: Provides JPMS name: {1}", Decorated.rpm(rpm), Decorated.actual(name));
             }
         }
