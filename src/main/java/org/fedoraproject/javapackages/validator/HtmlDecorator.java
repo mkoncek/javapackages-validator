1c983b9573582ee862aeefdcb95c4517c04522d7
Mikolaj Izdebski <mizdebsk@redhat.com>
2024-07-18 03:59:17 +0200

    Use CSS classes in HtmlDecorator

--- src/main/java/org/fedoraproject/javapackages/validator/HtmlDecorator.java
+++ src/main/java/org/fedoraproject/javapackages/validator/HtmlDecorator.java
@@ -13,47 +13,14 @@ class HtmlDecorator implements TextDecorator {
         super();
     }
 
-    private static String basic(Decoration.Color color) {
-        return switch (color) {
-            case black -> "color:black;";
-            case red -> "color:darkred;";
-            case green -> "color:darkgreen;";
-            case yellow -> "color:gold;";
-            case blue -> "color:darkblue;";
-            case magenta -> "color:darkmagenta;";
-            case cyan -> "color:darkcyan;";
-            case white -> "color:ghostwhite;";
-        };
-    }
-
-    private static String bright(Decoration.Color color) {
-        return switch (color) {
-            case black -> "color:darkgrey;";
-            case red -> "color:red;";
-            case green -> "color:green;";
-            case yellow -> "color:goldenrod;";
-            case blue -> "color:blue;";
-            case magenta -> "color:magenta;";
-            case cyan -> "color:#00e5ff;";
-            case white -> "color:white;";
-        };
-    }
-
     @Override
     public String decorate(Decorated decorated) {
-        var result = new StringBuilder("<text style=\"");
-        var color = decorated.getDecoration().color().orElse(Decoration.Color.black);
-        var colorString = basic(color);
+        var result = new StringBuilder("<text class=\"");
+        result.append(decorated.getDecoration().color().orElse(Decoration.Color.black));
 
         for (var modifier : decorated.getDecoration().modifiers()) {
-            switch (modifier) {
-                case bold -> {result.append("font-weight:bold;");}
-                case underline -> {result.append("text-decoration:underline;");}
-                case bright -> {colorString = bright(color);}
-            }
+            result.append(" ").append(modifier);
         }
-
-        result.append(colorString);
 
         result.append("\">");
         result.append(StringEscapeUtils.escapeHtml4(Objects.toString(decorated.getObject())).replace(System.lineSeparator(), "<br>"));
