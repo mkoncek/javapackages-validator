6539d69551ace7edc18c529c78f7201efef6071f
Mikolaj Izdebski <mizdebsk@redhat.com>
2024-11-13 12:00:45 +0100

    run.sh: Use compiled classes and dependencies
    This allows to run validator without packaging JAR file, eg. after
    making changes in an IDE.

--- run.sh
+++ run.sh
@@ -5,6 +5,4 @@ if [ -z "${JAVA_HOME:-}" ]; then
     JAVA_HOME=$(cat target/classes/JAVA_HOME)
 fi
 
-exec "${JAVA_HOME}"/bin/java --enable-native-access ALL-UNNAMED -jar 'target/validator.jar' "${@}"
-
-# exec "${JAVA_HOME}"/bin/java --enable-native-access ALL-UNNAMED -cp "target/classes$(find target/dependency -type f -printf ':%p')" org.fedoraproject.javapackages.validator.Main "${@}"
+exec "${JAVA_HOME}"/bin/java --enable-native-access ALL-UNNAMED -cp "target/classes:$(cat target/classpath)" org.fedoraproject.javapackages.validator.Main "${@}"
