bebb2059de7a5295f418dcabfa71cbcbf68c938a
Mikolaj Izdebski <mizdebsk@redhat.com>
2025-03-07 07:54:52 +0100

    Add markdown javadocs for common test utilities

--- src/test/java/org/fedoraproject/javapackages/validator/TestCommon.java
+++ src/test/java/org/fedoraproject/javapackages/validator/TestCommon.java
@@ -13,12 +13,23 @@ import org.fedoraproject.javapackages.validator.spi.TestResult;
 
 import io.kojan.javadeptools.rpm.RpmPackage;
 
-
+/// Utility class providing common test-related methods and constants.
 public class TestCommon {
+
+    /// Path prefix for RPM build resources.
     public static final Path RPMBUILD_PATH_PREFIX = Path.of("src/test/resources/rpmbuild");
+
+    /// Path prefix for RPM package files.
     public static final Path RPM_PATH_PREFIX = RPMBUILD_PATH_PREFIX.resolve(Path.of("RPMS"));
+
+    /// Path prefix for SRPM package files.
     public static final Path SRPM_PATH_PREFIX = RPMBUILD_PATH_PREFIX.resolve(Path.of("SRPMS"));
 
+    /// Creates an iterable collection of `RpmPackage` instances from the given paths.
+    ///
+    /// @param paths the paths to create `RpmPackage` instances from.
+    /// @return an iterable collection of `RpmPackage` instances.
+    /// @throws UncheckedIOException if an I/O error occurs while creating an `RpmPackage`.
     public static Iterable<RpmPackage> fromPaths(Path... paths) {
         var result = new ArrayList<RpmPackage>(paths.length);
         for (var path : paths) {
@@ -31,22 +42,37 @@ public class TestCommon {
         return result;
     }
 
+    /// Asserts that the given result has a test outcome of SKIP.
+    ///
+    /// @param result the test result to check.
     public static void assertSkip(Result result) {
         assertEquals(TestResult.skip, result.getResult(), "expected result: SKIP");
     }
 
+    /// Asserts that the given result has a test outcome of PASS.
+    ///
+    /// @param result the test result to check.
     public static void assertPass(Result result) {
         assertEquals(TestResult.pass, result.getResult(), "expected result: PASS");
     }
 
+    /// Asserts that the given result has a test outcome of INFO.
+    ///
+    /// @param result the test result to check.
     public static void assertInfo(Result result) {
         assertEquals(TestResult.info, result.getResult(), "expected result: INFO");
     }
 
+    /// Asserts that the given result has a test outcome of WARN.
+    ///
+    /// @param result the test result to check.
     public static void assertWarn(Result result) {
         assertEquals(TestResult.warn, result.getResult(), "expected result: WARN");
     }
 
+    /// Asserts that the given result has a test outcome of FAIL with exactly one failure event.
+    ///
+    /// @param result the test result to check.
     public static void assertFailOne(Result result) {
         assertEquals(TestResult.fail, result.getResult(), "expected result: FAIL");
         int count = 0;
@@ -58,6 +84,9 @@ public class TestCommon {
         assertEquals(1, count);
     }
 
+    /// Asserts that the given result has a test outcome of ERROR.
+    ///
+    /// @param result the test result to check.
     public static void assertError(Result result) {
         assertEquals(TestResult.error, result.getResult(), "expected result: ERROR");
     }
--- src/test/java/org/fedoraproject/javapackages/validator/TestFactory.java
+++ src/test/java/org/fedoraproject/javapackages/validator/TestFactory.java
@@ -8,39 +8,64 @@ import org.fedoraproject.javapackages.validator.spi.ValidatorFactory;
 
 import io.kojan.javadeptools.rpm.RpmPackage;
 
+/// Factory class for creating and managing validators.
 public class TestFactory implements ValidatorFactory {
 
+    /// List of registered validators.
     static List<Validator> validators = new ArrayList<>();
 
+    /// Retrieves a list of validators.
+    ///
+    /// @return a new list containing all registered validators.
     @Override
     public List<Validator> getValidators() {
         return new ArrayList<>(validators);
     }
-
 }
 
+/// Functional interface representing an anonymous validator.
 interface AnonymousValidator {
+
+    /// Validates a collection of RPM packages using the given default validator.
+    ///
+    /// @param rpms the iterable collection of RPM packages to validate.
+    /// @param rb   the default validator used for validation.
+    /// @throws Exception if any validation error occurs.
     void validate(Iterable<RpmPackage> rpms, DefaultValidator rb) throws Exception;
 }
 
+/// A test validator that extends the DefaultValidator and delegates validation to an AnonymousValidator.
 class TestValidator extends DefaultValidator {
 
+    /// Name of the test validator.
     final String name;
+
+    /// Delegate validator that performs the actual validation.
     final AnonymousValidator delegate;
 
+    /// Constructs a TestValidator with a given name and delegate validator.
+    ///
+    /// @param name     the name of the validator.
+    /// @param delegate the anonymous validator to delegate validation to.
     TestValidator(String name, AnonymousValidator delegate) {
         this.name = name;
         this.delegate = delegate;
     }
 
+    /// Retrieves the name of the test validator.
+    ///
+    /// @return the name of the test.
     @Override
     public String getTestName() {
         return name;
     }
 
+    /// Validates a collection of RPM packages by delegating to the anonymous validator.
+    ///
+    /// @param rpms the iterable collection of RPM packages to validate.
+    /// @throws Exception if any validation error occurs.
     @Override
     protected void validate(Iterable<RpmPackage> rpms) throws Exception {
         delegate.validate(rpms, this);
     }
-
 }
